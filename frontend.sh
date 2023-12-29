cbccb6f64c2b87b7125f59c0dfce7672270d810a
saidevops7722 <saiaws7722@gmail.com>
2023-12-29 18:59:21 +0530

    roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,32 @@
 source common.sh
 
 
-print_head " installing nginx "
+print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 status_check $?
 
-print_head " removing old content "
+print_head "removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
 status_check $?
 
-print_head " downloading frontend content "
+print_head "downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 status_check $?
 
-print_head " extracting downloaded frontend "
-cd /usr/share/nginx/html &>>${log_file}
+print_head "extracting downloaded frontend"
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 status_check $?
 
-print_head " copying nginx conf for roboshop "
+print_head "copying nginx conf for roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 status_check $?
 
-print_head " enabling nginx "
+print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
 status_check $?
 
-print_head " restarting nginx "
+print_head "restarting nginx"
 systemctl restart nginx &>>${log_file}
 status_check $?
 
