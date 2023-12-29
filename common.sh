41df2417c6edb29823514a24c452148edcd98a28
saidevops7722 <saiaws7722@gmail.com>
2023-12-29 18:54:50 +0530

    roboshop-shell

--- common.sh
+++ common.sh
@@ -1,8 +1,18 @@
 code_dir=$(pwd)
-
 log_file=/tmp/roboshop.log
 rm -rf ${log_file}
 
 print_head (){
   echo -e "\e35m$1\e[0m"
 }
+
+status_check(){
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
+}
+
+
+
