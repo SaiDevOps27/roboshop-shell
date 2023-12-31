c9a6123ef6bc6868c552edccabf37a07a0af8228
saidevops7722 <saiaws7722@gmail.com>
2023-12-31 13:23:55 +0530

    roboshop-shell

--- shipping.sh
+++ shipping.sh
@@ -2,8 +2,7 @@ source common.sh
 
 mysql_root_password=$1
 
-
- if [ -z "${mqysql_root_password}" ]; then
+ if [ -z "${mysql_root_password}" ]; then
    echo -e "\e[31m missing mysql root password argument\e[0m"
    exit 1
  fi
